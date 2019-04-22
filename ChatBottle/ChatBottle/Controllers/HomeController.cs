b45832cb068863170604a8c1ba2bf8f7d707a480
wolfworker <1195747247@qq.com>
2019-04-22 18:23:50 +0800

    1：项目结构

---
+++ ChatBottle/ChatBottle/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ChatBottle.Models;
+
+namespace ChatBottle.Controllers
+{
+    public class HomeController : Controller
+    {
+        public DataBaseContext DbContext;
+
+        public HomeController(DataBaseContext DBContext)
+        {
+            DbContext = DBContext;
+        }
+        public IActionResult Index()
+        {
+            var customers = DbContext.Customer.ToList();
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ChatBottle/ChatBottle/Models/DataBaseContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatBottle.Models
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Customer> Customer { get; set; }
+    }
+}
