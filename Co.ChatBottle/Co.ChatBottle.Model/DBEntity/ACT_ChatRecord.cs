c3a9880e9f040100914605bf0b2644a4e52ff953
wolfworker <1195747247@qq.com>
2019-05-06 00:00:20 +0800

    1：优化 2：websocket 聊天

---
+++ Co.ChatBottle/Co.ChatBottle.Model/DBEntity/ACT_ChatRecord.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Co.ChatBottle.Model
+{
+    public class ACT_ChatRecord : CommonEntity
+    {
+        public string ID { get; set; }
+        public long BottleID { get; set; } = 0;
+        public long SenderID { get; set; } = 0;
+        public long ReceiverID { get; set; } = 0;
+        public string ChatText { get; set; }
+        public byte Status { get; set; }
+        public string Remark { get; set; }
+    }
+}
